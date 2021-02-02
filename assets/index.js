99ce13460e820770e849fb501a537d48f265b14a
Rachel Amos <rachelamos35@gmail.com>
2021-02-02 00:36:56 -0600

    adding forecast html, JS, and CSS

--- assets/index.js
+++ assets/index.js
@@ -10,9 +10,14 @@ var windSpeed = document.querySelector('.windSpeed');
 var uvIndex = document.querySelector('.uvIndex');
 var temp1 = document.querySelector('.temp1');
 var humidity1 = document.querySelector('.humidity1');
-var day3 = document.querySelector('.day3');
-var day4 = document.querySelector('.day4');
-var day5 = document.querySelector('.day5');
+var temp2 = document.querySelector('.temp2');
+var humidity2 = document.querySelector('.humidity2');
+var temp3 = document.querySelector('.temp3');
+var humidity3 = document.querySelector('.humidity3');
+var temp4 = document.querySelector('.temp4');
+var humidity4 = document.querySelector('.humidity4');
+var temp5 = document.querySelector('.temp5');
+var humidity5 = document.querySelector('.humidity5');
 
 // Fetch information on city from OpenWeather API
 
@@ -50,7 +55,7 @@ function uvFunction (lat, lon){
 };
 
 function forecast (lat, lon){
-  fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=alerts,minutely,hourly,&appid=e6ba2c290e501b458abfcf542542980c')
+  fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=alerts,minutely,hourly,&appid=e6ba2c290e501b458abfcf542542980c&units=imperial')
   .then(function (response){
     console.log(response);
     return response.json();
@@ -60,6 +65,14 @@ function forecast (lat, lon){
     // day1.textContent = "" + data.daily[0].dt;
     temp1.textContent = "Temp: " + data.daily[1].temp.day + "°F";
     humidity1.textContent = "Humidity: " + data.daily[1].humidity + "%";
+    temp2.textContent = "Temp: " + data.daily[2].temp.day + "°F";
+    humidity2.textContent = "Humidity: " + data.daily[2].humidity + "%";
+    temp3.textContent = "Temp: " + data.daily[3].temp.day + "°F";
+    humidity3.textContent = "Humidity: " + data.daily[3].humidity + "%";
+    temp4.textContent = "Temp: " + data.daily[4].temp.day + "°F";
+    humidity4.textContent = "Humidity: " + data.daily[4].humidity + "%";
+    temp5.textContent = "Temp: " + data.daily[5].temp.day + "°F";
+    humidity5.textContent = "Humidity: " + data.daily[5].humidity + "%";
   })
 };
 
